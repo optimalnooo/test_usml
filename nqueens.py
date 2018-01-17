4068c022d747d9b1b0ddb6be6cfb486a86bcc865
optimalnooo <optimalnooo@gmail.com>
2018-01-17 23:07:17 +0500

    fix blank lines

--- nqueens.py
+++ nqueens.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 import numpy as np
 
@@ -6,22 +7,19 @@ class Solver_8_queens:
     DIM_SIZE = 8
     DIMENSION = 2
 
-    def __init__(self):
+    def __init__(self, pop_size=100, cross_prob=0.5, mut_prob=0.25):
         pass
 
+    def solve(self, min_fitness=0.9, max_epochs=100):
+        pass
 
     def create_start_population(self):
         pass
 
-
     def get_selected_individual(self, population):
-        '''
-        roulette wheel
-        '''
-
+        '''roulette wheel'''
         weights = [self.fitness(individ) for individ in population]
         weights_sum = sum(weights)
-
         rand_value = random.random() * weights_sum
 
         for i, w in enumerate(weights):
@@ -29,34 +27,26 @@ class Solver_8_queens:
             if rand_value <=0:
                 return population[i]
 
-
     def fitness(self, individual):
+        '''fitness function
+        return:
+          int value - from 0 to DIM_SIZE * (DIM_SIZE - 1) / 2
+          the more the better
         '''
-        fitness function
-        
-        return
-            int value - from 0 to DIM_SIZE * (DIM_SIZE - 1) / 2
-                        the more the better
-        '''
-
         count = 0
         for i in range(Solver_8_queens.DIM_SIZE):
             for j in range(i+1, Solver_8_queens.DIM_SIZE):
                 if self.check_pair_chromosomes(individual[i], individual[j]):
                     count += 1
-
         return count
 
 
     def check_pair_chromosomes(self, chrm1, chrm2):
-        '''
-        check chromosomes
-
+        '''check chromosomes
         return:
-            True  - if chromosomes not intersect
-            False - if chromosomes intersect
+          True  - if chromosomes not intersect
+          False - if chromosomes intersect
         '''
-
         bias1 = chrm1[0] - chrm2[0]
         bias2 = chrm1[1] - chrm2[1]
 
@@ -65,31 +55,29 @@ class Solver_8_queens:
         else:
             return True
 
-
     def get_init_population(self, size):
         population = [self.get_random_individual() for _ in range(size)]
         return population
 
-
     def get_random_individual(self):
-        individual = [self.get_random_chromosome() for _ in range(Solver_8_queens.DIM_SIZE)]
+        individual = [self.get_random_chromosome()
+            for _ in range(Solver_8_queens.DIM_SIZE)]
         return individual
 
-
     def get_random_chromosome(self):
-        chromosome = random.choices(range(Solver_8_queens.DIM_SIZE), k=Solver_8_queens.DIMENSION)
+        chromosome = random.choices(
+            range(Solver_8_queens.DIM_SIZE),
+            k=Solver_8_queens.DIMENSION
+        )
         return chromosome
 
-
     def show_individual(self, individual):
-        '''
-        individual vizualization
-        '''
-
-        display = [['_' for _ in range(Solver_8_queens.DIM_SIZE)] for _ in range(Solver_8_queens.DIM_SIZE)]
+        '''individual vizualization'''
+        display = [['+' for _ in range(Solver_8_queens.DIM_SIZE)]
+            for _ in range(Solver_8_queens.DIM_SIZE)
+        ]
         for chrm in individual:
-            display[chrm[0]][chrm[1]] = '#'
-
+            display[chrm[0]][chrm[1]] = 'Q'
         #show
         for i in range(Solver_8_queens.DIM_SIZE):
             for j in range(Solver_8_queens.DIM_SIZE):
@@ -105,3 +93,6 @@ def main():
     print(indiv)
 
     print(q.fitness(indiv))
+
+    #q->solver
+    #best_fit, epoch_num, visualization = solver.solve()
