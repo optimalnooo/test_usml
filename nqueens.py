bfc0be7c1d06268a112832826350a624d9154a0a
optimalnooo <optimalnooo@gmail.com>
2018-01-21 11:05:37 +0500

    upgrade fitness function

--- nqueens.py
+++ nqueens.py
@@ -5,6 +5,8 @@ import numpy as np
 class Solver_8_queens:
 
     DIM_SIZE = 8
+    FOUND_DECISION = False
+    RESULT = 0
 
     def __init__(self, pop_size=100, cross_prob=0.5, mut_prob=0.25):
         pass
@@ -12,34 +14,40 @@ class Solver_8_queens:
     def solve(self, min_fitness=0.9, max_epochs=100):
         pass
 
-    def get_selected_individual(self, population):
+    def get_selected_individual(self, population, weights, weights_sum):
         '''roulette wheel'''
-        weights = [self.fitness(individ) for individ in population]
-        weights_sum = sum(weights)
         rand_value = random.random() * weights_sum
-
         for i, w in enumerate(weights):
             rand_value -= w
             if rand_value <=0:
                 return population[i]
 
-    def fitness(self, individual):
+    def fitness_population(self, population):
         '''fitness function
         return:
-          int value - from 0 to DIM_SIZE * (DIM_SIZE - 1) / 2
-          the more the better
+          int array - weights array for each individual
+          int weights_sum - sum of weights
+
+          weight in range from 0 to DIM_SIZE * (DIM_SIZE - 1) / 2
+          the more weight the better
         '''
-        count = 0
-        for i in range(Solver_8_queens.DIM_SIZE):
-            for j in range(i+1, Solver_8_queens.DIM_SIZE):
-                if self.check_pair_chromosomes(individual[i], individual[j]):
-                    count += 1
-        if count == (Solver_8_queens.DIM_SIZE * (Solver_8_queens.DIM_SIZE - 1) / 2):
-            self.show_individual(individual)
-        return count
+        weights = []
+        for individual in population:
+            count = 0
+            for i in range(Solver_8_queens.DIM_SIZE):
+                for j in range(i+1, Solver_8_queens.DIM_SIZE):
+                    if self.check_pair_chromosomes([i, individual[i]], [j, individual[j]]):
+                        count += 1
+            if count == (Solver_8_queens.DIM_SIZE * (Solver_8_queens.DIM_SIZE - 1) / 2):
+                Solver_8_queens.RESULT += 1
+                #Solver_8_queens.FOUND_DECISION = True
+                #return None, None
+            weights.append(count)
+        weights_sum = sum(weights) 
+        return weights, weights_sum
 
     def crossover(self, indiv1, indiv2):
-        crossover_point = np.random.randint(0, 8)
+        crossover_point = np.random.randint(0, Solver_8_queens.DIM_SIZE)
         new_indiv1 = np.concatenate((
             indiv1[:crossover_point],
             indiv2[crossover_point:]
@@ -85,15 +93,29 @@ def main():
     
     q = Solver_8_queens()
 
-    indiv1 = q.get_random_individual()
-    indiv2 = q.get_random_individual()
-    new1, new2 = q.crossover(indiv1, indiv2)
+    ar = []
+    for i in range(10000):
+        indiv = q.get_random_individual()
+        ar.append(indiv)
+    print('Start population was created')
+    new_ar = []
+    weights, weights_sum = q.fitness_population(ar)
 
-    print(indiv1)
-    print(indiv2)
-    print()
-    print(new1)
-    print(new2)
+    for i in range(10000):
+        ind1 = q.get_selected_individual(ar, weights, weights_sum)
+        ind2 = q.get_selected_individual(ar, weights, weights_sum)
+        a,b = q.crossover(ind1, ind2)
+        new_ar.append(a)
+        new_ar.append(b)
+        if i % 1000 == 0:
+            print(i)
+    print("Was success: ", end='')
+    print(Solver_8_queens.RESULT)
+    Solver_8_queens.RESULT = 0
+
+    print("Now success: ", end='')
+    q.fitness_population(new_ar)
+    print(Solver_8_queens.RESULT)
 
     #q->solver
     #best_fit, epoch_num, visualization = solver.solve()
